7b802d7beb86ff925d8fb5853017cead259336e7
JordanB <114504655+joleb24@users.noreply.github.com>
2022-10-02 16:43:56 +0200

    Update settings.py

--- posada/settings.py
+++ posada/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x5yuy^$o(+-)$*i=h*4)r9dh@ci62jo^gku4^mlhfw*1t7!k+%'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
